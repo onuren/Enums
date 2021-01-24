4b3df9f92bdc0a72f4c99f2348d7f8013e67f73d
onur engun <onurengun@outlook.com>
2021-01-25 01:54:03 +0300

    Add project files.

---
+++ DAL/DataModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DataModel
+    {
+        SqlConnection con; SqlCommand cmd;
+
+        public DataModel()
+        {
+            con = new SqlConnection(ConnectionString.ConStr);
+            cmd = con.CreateCommand();
+        }
+
+        public ModelSonuc KategoriSil(int id)
+        {
+            try
+            {
+                cmd.CommandText = "SELECT COUNT(*) FROM Products WHERE CategoryID=@id";
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@id", id);
+                con.Open();
+                int sayi = Convert.ToInt32(cmd.ExecuteScalar());
+                if (sayi == 1)
+                {
+                    cmd.CommandText = "DELETE FROM Categories WHERE CategoryID=@id";
+                    cmd.Parameters.Clear();
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                    return ModelSonuc.Basarili;
+                }
+                else
+                {
+                    return ModelSonuc.Kullanimda;
+                }
+            }
+            catch(Exception ex)
+            {
+                return ModelSonuc.Basarisiz;
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
---
+++ EnumKavrami/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace EnumKavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Tanımlama
+
+            //Kimi zaman kodların daha kolay yazılmasını ve okunmasını sağlarken çoğunlukla hata yapılmasını engellemek için kullanılır.
+
+            //.Net Framework üzerinde hazır enumlar olduğu gibi kullanıcı tanımlı enumlar da hazırlanabilir.
+
+
+            #endregion
+
+            #region Kullanım
+            //Console.WriteLine("Bugün = " + HaftaninGunleri.cumartesi);
+            //Console.WriteLine("Haftanın = " + (int)HaftaninGunleri.cumartesi + ". günü");
+            //Console.WriteLine((HaftaninGunleri)6);
+            #endregion
+
+            #region Kullanım 2
+
+            DataModel db = new DataModel();
+            //Console.WriteLine(db.KategoriSil(3));
+            ModelSonuc ms = db.KategoriSil(24);
+
+            switch (ms)
+            {
+                case ModelSonuc.Basarili:
+                    Console.WriteLine("Kategori silme işlemi başarılı");
+                    break;
+                case ModelSonuc.Basarisiz:
+                    Console.WriteLine("Kategori silme işlemi başarısız");
+                    break;
+                case ModelSonuc.Kullanimda:
+                    Console.WriteLine("Silinmek istenen kategori kullanımda");
+                    break;
+                default:
+                    break;
+            }
+            #endregion
+        }
+    }
+    public enum HaftaninGunleri
+    {
+        pazartesi = 1,
+        sali,
+        carsamba,
+        persembe,
+        cuma,
+        cumartesi,
+        pazar
+    }
+}
